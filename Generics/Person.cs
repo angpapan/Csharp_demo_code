bc9b63e2e4d1d2c62858712eef041e17ede58f4e
angpapan <61470994+angpapan@users.noreply.github.com>
2024-04-23 11:35:09 +0300

    Add files via upload

---
+++ Generics/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace Generics;
+
+internal class Person<T>
+{
+    private T _id;
+    public T Id => _id;
+    public string Name { get; set; }
+    public string Title { get; set; }
+
+    public Person(T id)
+    {
+        _id = id;
+    }
+
+    public Person(T id, string name, string title) : this(id)
+    {
+        Name = name;
+        Title = title;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {_id}, Title: {Title}, Name: {Name}";
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,13 @@
+﻿using Generics;
+
+Person<int> galdalf = new Person<int>(1, "Gandalf", "The White");
+Person<string> thranduil = new Person<string>("THRNDL", "Thranduil", "King of the Elves");
+Person<double> aragorn = new Person<double>(100.12);
+aragorn.Name = "Aragorn";
+aragorn.Title = "King of Gondor";
+
+Console.WriteLine(galdalf);
+Console.WriteLine(thranduil);
+Console.WriteLine(aragorn);
+
+
