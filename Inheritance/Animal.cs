3cf46540bae61eff64da3db8b4f6af90edcc3c53
angpapan <61470994+angpapan@users.noreply.github.com>
2024-04-24 13:23:31 +0300

    Add IsAlive method to animal

--- Inheritance/Animal.cs
+++ Inheritance/Animal.cs
@@ -4,8 +4,13 @@ internal abstract class Animal
 {
     private int _age = 0;
     public int Age => _age;
+    public int Lifespan {get; }
     public Family Family { get; }
-    protected Animal(Family family) { Family = family; }
+    protected Animal(Family family, int lifespan) 
+    { 
+        Family = family; Lifespan = lifespan;
+    }
     public void IncreaseAge() => _age++;
     public abstract void MakeSound();
+    public bool IsAlive() => Age <= Lifespan;
 }
