9cbdab4fd6d9e058a5da22c740c61302299eebd5
zkammas <zkammas21@gmail.com>
2024-05-09 16:14:45 +0300

    simple class illustration

---
+++ Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    internal class Animal
+    {
+        public int legsNum;
+    }
+
+    static void Main(string[] args)
+    {
+        // Creating a new instance of the Animal class
+        Animal dog = new Animal();
+        dog.legsNum = 4;
+        // Another instance of the Animal class
+        Animal snake = new Animal();
+        snake.legsNum = 0;
+        Console.WriteLine("Dog has {0} legs", dog.legsNum);
+        Console.WriteLine("Snake has {0} legs" ,snake.legsNum);
+    }
+}
+
+ 
