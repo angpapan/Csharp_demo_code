fa9ba560f7ca931454ef6c298d12d9dc936cd684
angpapan <61470994+angpapan@users.noreply.github.com>
2024-05-12 19:46:33 +0300

    Update Team.cs add delegate function

--- Delegates/FunctionalProgramming/Team.cs
+++ Delegates/FunctionalProgramming/Team.cs
@@ -1,8 +1,14 @@
-﻿namespace Delegates.FunctionalProgramming;
+namespace Delegates.FunctionalProgramming;
 
 internal class Team
 {
     public string Name { get; init; }
     public List<Player> Players { get; set; } = new();
     public Team(string name) { Name = name; }
+    public void PrintPlayers(Predicate<Player> criterion)
+    {
+        foreach(Player p in Players)
+            if(criterion(p)) Console.WriteLine(p);
+    }
+    
 }
