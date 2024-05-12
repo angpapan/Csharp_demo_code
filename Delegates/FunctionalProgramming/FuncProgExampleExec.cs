0a5cbd209c3f37a3c578556df3fefd38b5f864dc
angpapan <61470994+angpapan@users.noreply.github.com>
2024-05-12 19:45:57 +0300

    Update FuncProgExampleExec.cs

--- Delegates/FunctionalProgramming/FuncProgExampleExec.cs
+++ Delegates/FunctionalProgramming/FuncProgExampleExec.cs
@@ -1,6 +1,5 @@
-﻿namespace Delegates.FunctionalProgramming;
+namespace Delegates.FunctionalProgramming;
 
-delegate bool PlayerDelegate(Player player);
 delegate bool TeamDelegate(Team team); // == Predicate<Team>
 
 internal class FuncProgExampleExec
@@ -11,17 +10,17 @@ internal class FuncProgExampleExec
         List<Player> players = DataFactory.CreatePlayers(teams);
         players.ForEach(p => p.Team.Players.Add(p));
 
-        PrintPlayers(p => p.HeightInCm > 205);
+        Console.WriteLine($"{teams[1].Name} players over 205cm");
+        teams[1].PrintPlayers(p => p.HeightInCm > 205);
         Console.WriteLine();
+        Console.WriteLine($"{teams[1].Name} players under 203cm");
+        teams[1].PrintPlayers(p => p.HeightInCm < 203);
+        Console.WriteLine();
+        Console.WriteLine($"Players playing for Olympiakos");
         PrintPlayersByTeamCriterion(t => t.Name == "Olympiakos");
 
-        void PrintPlayers(PlayerDelegate criterion)
-        {
-            foreach (Player player in players)
-                if (criterion(player)) Console.WriteLine(player);
-        }
 
-        void PrintPlayersByTeamCriterion(Predicate<Team> criterion)
+        void PrintPlayersByTeamCriterion(TeamDelegate criterion)
         {
             foreach (Player player in players)
                 if (criterion(player.Team)) Console.WriteLine(player);
