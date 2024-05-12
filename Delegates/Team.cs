c51eb5c4851a4d610a941c1c153342aae9214195
angpapan <61470994+angpapan@users.noreply.github.com>
2024-05-12 18:33:54 +0300

    Delete Delegates directory

--- Delegates/Team.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Delegates;
-
-internal class Team
-{
-    public string Name { get; init; }
-    public List<Player> Players { get; set; } = new();
-    public Team(string name) { Name = name; }
-}
